05d26ff1697bdca928dd25b89ee8989143f1b9c1
Dago Moreno <113307225+Dago1026@users.noreply.github.com>
2023-06-26 03:20:33 +0200

    26/6
    ejercicios java script y boton toggle bar

---
+++ Java Script/Modulo 2/3 producto.js
@@ -0,0 +1,11 @@
+function calcPrecio(){
+    let nuevoPrecio = 0 ;
+    for(let i = 0; i < 10; i++){
+    let precioProducto = parseFloat(prompt(`ingrese el precio del producto ${i} `));
+    nuevoPrecio = nuevoPrecio + precioProducto ;
+    }
+    console.log(nuevoPrecio);
+    alert(nuevoPrecio);
+}
+
+calcPrecio();
---
+++ Java Script/Modulo 2/4 notas.js
@@ -0,0 +1,28 @@
+function notasEstudiante(){
+    let contador1 = 0 ;
+    let contador2 = 0 ;
+    let contador3 = 0 ;
+    for(let i = 0; i < 3; i++){
+    let nota = parseFloat(prompt("ingrese la nota entre 0  y  10"));
+    if (nota<0 ) {
+        
+       contador3 ++; 
+        
+    }else if (nota<6){
+           contador1 ++;
+    }
+    
+    
+    else if(nota<11){
+         contador2 ++;  
+    }else{
+        contador3 ++;
+    }
+  
+
+}
+
+alert(" Reprobados son : " + contador1 + "  y  Aprobados son   " + contador2 + "  Notas Erradas: " + contador3);
+}
+
+notasEstudiante(); 
